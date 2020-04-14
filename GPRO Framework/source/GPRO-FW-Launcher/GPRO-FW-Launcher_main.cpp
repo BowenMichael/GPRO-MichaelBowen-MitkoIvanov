8251e35c9c743711e9d518de832739f6318c6097
Michael Bowen <michael.bowen@mymail.champlain.edu>
2020-04-14 13:35:22 -0400

    Guessing game

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
@@ -5,13 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+//#include "../GPRO-FW/GPRO-FW.h"
 using namespace std;
 
 int main()
 {
-	
+	int guess = 50;
+	string input = "y";
 	cout << "I am going to guess your age Just tell me if I am higher or lower or correct." << endl;
-	/*do {
-
-	} while ();*/
+	do {
+		cout << "I am think your age is: " << guess << ". Is it higher lower or did I get it right?";
+		cin >> input;
+		//guess = ageGuesser(input, guess);
+	} while (input != "y");
 }
--- GPRO Framework/source/GPRO-FW/ageGuesser.cpp
+++ GPRO Framework/source/GPRO-FW/ageGuesser.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
 #include <string>
 using namespace std;
 
-int ageGusser(string input,int guess) {
+const int MAX_AGE = 150;
+const int MIN_AGE = 0;
 
+int ageGusser(string input, int guess) {
+	switch (input.at(0)) {
+	case 'h':
+		cout << "Ok let me try again";
+		return guess + ((MAX_AGE - guess) / 2);
+		break;
+	case 'l':
+		cout << "Ok let me try again";
+		return guess - ((guess - MIN_AGE) / 2);
+		break;
+	case 'c':
+		cout << "Yes I am so smart!" << endl;
+		return -1;
+		break;
+	}
 }
